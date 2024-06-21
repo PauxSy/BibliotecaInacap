68ca579c9aae2c5aa5e40d890209202013bcd351
Franciscaii <francisca.figueroaer@gmail.com>
2024-06-20 21:07:25 -0400

    agregar libros

---
+++ codigo_eva4/libros.py
@@ -0,0 +1,8 @@
+class Libro:
+    def __init__(self, ISBN_Libro: int, Genero: str, Titulo: str, Autor: str, Stock: int, Idioma: str):
+        self.ISBN_Libro = ISBN_Libro
+        self.Genero = Genero
+        self.Titulo = Titulo
+        self.Autor = Autor
+        self.Stock = Stock
+        self.Idioma = Idioma
